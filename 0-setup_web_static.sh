d6bc330e989a2bc8fcf49add9ad4b8e3de3faf66
kokole12 <kokoleismail886@gmail.com>
2023-04-05 18:10:44 +0000

    configuration for deploying web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,7 @@ Holberton School
 </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/we_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
+chown -R ubuntu:ubuntu /data/
 sudo sed -i 'listen 80 default_server/a \tlocation /hbnb_static {\n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
 sudo service nginx restart
 

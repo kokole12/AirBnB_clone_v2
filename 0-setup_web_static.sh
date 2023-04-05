7fda04987e2f51335897adc58b8afb89758fb822
kokol12 <kokoleismail886@gmail.com>
2023-04-05 21:19:30 +0300

    setting up webserver

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,15 +6,15 @@ sudo ufw allow "Nginx HTTP"
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "<html>
-<head>
-</head>
-<body>
-Holberton School
-</body>
+sudo echo
+"<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/we_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sudo sed -i 'listen 80 default_server/a \tlocation /hbnb_static {\n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
 sudo service nginx restart
-

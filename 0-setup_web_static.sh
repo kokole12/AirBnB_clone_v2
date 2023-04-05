6c0c0fa7a2314c2a9cf4ce63d2128f5e50f0eca1
kokole12 <kokoleismail886@gmail.com>
2023-04-05 13:14:08 +0000

    shell script to set up web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Bash script to set up a web server for deployment of webstatic
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+<head>
+</head>
+<body>
+Holberton School
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbtn_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart

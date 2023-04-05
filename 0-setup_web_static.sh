7ad548b9c705467c14a18f497586e8f3bfcb4323
kokol12 <kokoleismail886@gmail.com>
2023-04-05 21:22:21 +0300

    setting up webserver

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,5 +16,5 @@ sudo echo
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/we_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i 'listen 80 default_server/a \tlocation /hbnb_static {\n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
+sudo sed -i 'listen 80 default_server/a location /hbnb_static {\n\t\t alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
 sudo service nginx restart

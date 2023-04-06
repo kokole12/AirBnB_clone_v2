708669f3b970751da512aa1cde92d56711c1bf4f
kokole12 <kokoleismail886@gmail.com>
2023-04-06 08:55:21 +0000

    changes applied to files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,13 +8,13 @@ from datetime import date
 def do_pack():
     """ A script that generates archive the contents of web_static folder"""
 
-    filename = strftime("%Y%m%d%H%M%S")
+    filetime = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
+              .format(filetime))
 
-        return "versions/web_static_{}.tgz".format(filename)
+        return "versions/web_static_{}.tgz".format(filetime)
 
     except Exception as e:
         return None

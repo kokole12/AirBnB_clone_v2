0727614a86b8916eda70abd83256d8a1091fbc08
kokol12 <kokoleismail886@gmail.com>
2023-04-06 14:39:10 +0300

    deploying webstatic

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-""" setting up to generate a .tgz fiel"""
-from fabric.api import local
-from time import strftime
+"""
+fabric scrip to create .tgz file
+"""
+
+from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
-    """setting up the local directory """
-    try:
-        local('mkdir -p versions')
-        date = strftime("%Y%m%d%H%M%S")
-        """ creating the file"""
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(date))
-        return ('versions/web_static_{}.tgz'.format(date))
-    except Exception as e:
+    """
+    making an archive on web_static folder
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None

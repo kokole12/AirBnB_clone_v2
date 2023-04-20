cfd9967681f4fa824be8053da13162dcbb12748a
kokole12 <kokoleismail886@gmail.com>
2023-04-20 13:42:18 +0000

    working with fist template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -25,9 +25,14 @@ def c_isfun(text):
 def python(text='is_cool'):
 	return ('Python {}'.format(escape(text.replace("_", " "))))
 
-@app.route('/number/<int:n>')
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-	return render_template('5-number.html', n=n)
+	return ('{} is a number'.format(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_html(n):
+	return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":

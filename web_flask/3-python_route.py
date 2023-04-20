c5e72edaf58e776eb23f23bb9e70453677794ae3
kokol12 <kokoleismail886@gmail.com>
2023-04-20 15:35:59 +0300

    fixed errot

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c_isfun(text):
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-	return ('Python {}'.format(escape(text.replace("_", " ")))
+	return ('Python {}'.format(escape(text.replace("_", " "))))
 
 
 if __name__ == "__main__":

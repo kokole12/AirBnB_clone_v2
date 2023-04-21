fe2a2a28ffb99e54136701660eedbf4a7e5e0acf
kokol12 <kokoleismail886@gmail.com>
2023-04-21 20:00:07 +0300

    cities_by_state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/uar/bin/python3
+"""
+    Python flask web app script to display
+    the states and the cities
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_state(states):
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states = states)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)

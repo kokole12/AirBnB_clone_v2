de18d22cafda9d2e5d4271f6331bfc287eca0478
kokol12 <kokoleismail886@gmail.com>
2023-04-21 11:24:55 +0300

    fetching states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Python flask script to print a list of states
+"""
+
+from flask import Flask, render_template, g
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/state_list', strict_slahes=False)
+def state_list(states):
+    states = storage.all()
+    return render_template(states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)

68595e463bc1e9e901cddf4e0f3728687b54bae5
kokol12 <kokoleismail886@gmail.com>
2023-04-21 15:39:54 +0300

    pep8 formated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,7 +34,8 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_html(n):
-    return render_template("5-number.html", n=n)
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":

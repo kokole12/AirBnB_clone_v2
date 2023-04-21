cbdd213d8b1f2621ae3a75af0e866ea77d14a0f9
kokol12 <kokoleismail886@gmail.com>
2023-04-21 15:32:57 +0300

    fixed error

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,24 +7,24 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-	return ("Hello HBNB!")
+    return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
-	return ('HBNB')
+    return ('HBNB')
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_isfun(text):
-	return ('C {}'.format(escape(text.replace("_", " "))))
+    return ('C {}'.format(escape(text.replace("_", " "))))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is_cool'):
-	return ('Python {}'.format(escape(text.replace("_", " "))))
+    return ('Python {}'.format(escape(text.replace("_", " "))))
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)

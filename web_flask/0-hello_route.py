59d82984494f9f9b31950629ebbcbdff39858914
kokol12 <kokoleismail886@gmail.com>
2023-04-21 15:24:00 +0300

    edited first file to pep8 standard

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-	""" Return this to get request"""
-	return ("Hello HBNB!")
+    """ Return this to get request"""
+    return ("Hello HBNB!")
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)

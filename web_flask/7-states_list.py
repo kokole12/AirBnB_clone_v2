2ecddeec4b2b563db5074eb3b809ef1aea7f2a96
kokol12 <kokoleismail886@gmail.com>
2023-04-22 20:15:49 +0300

    updating files to finx bugs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def state_list():
-    states = storage.all(State)
+    states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 

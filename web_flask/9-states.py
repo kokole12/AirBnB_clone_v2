b120b373dc9a5ced34ed0b8de0fc6f17f18632d8
kokol12 <kokoleismail886@gmail.com>
2023-04-22 20:43:36 +0300

    debugginh

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -15,12 +15,12 @@ app = Flask(__name__)
 @app.route('/states', strict_slashes=True)
 def states():
     states = storage.all(State)
-    return render_template("9-states.html", states=states, query_width='all')
+    return render_template("9-states.html", states=states, query_with='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_by_id(id):
-    for state in storage.all(State).values():
+    for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", query_with='id')
         return render_template("9-states.html", states=state, query_with=None)
